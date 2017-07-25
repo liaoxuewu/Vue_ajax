ad5f8009253e31776565137ad89e3ac59ec2bd1f
liaoxuewu <liaoxuewu1993@163.com>
2017-07-25 23:51:37 +0800

    complete

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,13 +2,31 @@
   <section class="jumbotron">
     <h3 class="jumbotron-heading">Search Github Users</h3>
     <div>
-      <input type="text" placeholder="enter the name you search"/>
-      <button>Search</button>
+      <input type="text" placeholder="enter the name you search" v-model="inputName"/>
+      <button @click="search">Search</button>
     </div>
   </section>
 </template>
 <script>
-
+  export default {
+    props:['setSearchName'],
+    data () {
+      return {
+        inputName:''
+      }
+    },
+    methods: {
+      search () {
+        //获取用户输入的值
+        const inputName = this.inputName.trim();
+        //检查用户输入的值
+        if(!inputName){
+          return
+        }
+        this.setSearchName(inputName);
+      }
+    }
+  }
 </script>
 <style>
 
